a6c3ff6a6a982a426c3f321ab335a5dedf755cad
Stefan Krabbe <stefankrabbe54@gmail.com>
2017-10-23 11:13:10 +0200

    Testing if it is possible to commit from sourcetree

--- BusinessLogic/Mail.cs
+++ BusinessLogic/Mail.cs
@@ -10,6 +10,8 @@ namespace BusinessLogic
 {
     public class Mail
     {
+        private string troll = "null";
+
         public IRestResponse SendSimpleMessage()
         {
             RestClient client = new RestClient();
