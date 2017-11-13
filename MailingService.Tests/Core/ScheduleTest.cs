f46766984bfb7b7b49e46128ab546fe9309e5bb1
T. Andersen <Tzarreth@users.noreply.github.com>
2017-11-13 09:00:47 +0100

    Test start
    Starting on test for SV core

---
+++ MailingService.Tests/Core/ScheduleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailingService.Tests.Core
+{
+    [TestClass]
+    public class ScheduleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
