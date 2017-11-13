3248eea5648663f827e254c9e4b328ce0e526b0f
T. Andersen <Tzarreth@users.noreply.github.com>
2017-11-13 15:40:20 +0100

    Schedule
    Schedule Services implemented, I think.

--- Domain/Shift.cs
+++ Domain/Shift.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core
 {
+    [DataContract]
     public class Shift
     {
+        [DataMember]
         public DateTime StartTime { get; set; }
+
+        [DataMember]
         public int Hours { get; set; }
+
+        [DataMember]
         public Employee Employee { get; set; }
 
         public Shift()
