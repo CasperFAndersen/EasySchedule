186dafbe5899bf2b4e482a51a02cd76149f9c2f5
T. Andersen <Tzarreth@users.noreply.github.com>
2017-11-14 09:16:19 +0100

    ServiceHost

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WCF EmployeeService is now running.");
+
+            using (ServiceHost host = new ServiceHost(typeof(EmployeeService))) ;
+            {
+                host.Open();
+                Console.WriteLine("Service is now running");
+                Console.ReadLine();
+            }
+        }
+    }
+}
