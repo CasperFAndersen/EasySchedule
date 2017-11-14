13742028250403bda4914b801bae7d984236c1ab
Casper Froberg Andersen <casperfroberg@gmail.com>
2017-11-14 12:19:29 +0100

    Fixed service
    fixed service med ordenlig connectionstring

--- DatabaseAccess/TempScheduleDB.cs
+++ DatabaseAccess/TempScheduleDB.cs
@@ -11,61 +11,61 @@ namespace DatabaseAccess
 {
     public class TempScheduleDB
     {
-        
+        DbConnectionADO dbConADO = new DbConnectionADO();
+
         public IEnumerable<TempSchedule> GetAll()
         {
             List<TempSchedule> tempList = new List<TempSchedule>();
-            using (DbConnectionADO dBCon = new DbConnectionADO())
+            using (SqlConnection dBCon = new SqlConnection(dbConADO.KrakaConnectionString()))
             {
-                dBCon.OpenConnection();
-                //TODO: Uncomment
-                //SqlCommand command = new SqlCommand("SELECT * FROM TempSchedule", dBCon);
-                //using (DbDataReader reader = command.ExecuteReader())
-                //{
-                //    while (reader.Read())
-                //    {
-                //        TempSchedule tempSchedule = new TempSchedule(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2));
-                //        tempList.Add(tempSchedule);
-                //    }
-                //}
-                dBCon.CloseConnection();
+                dBCon.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM TempSchedule", dBCon);
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        TempSchedule tempSchedule = new TempSchedule(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2));
+                        tempList.Add(tempSchedule);
+                    }
+                }
+                dBCon.Close();
             }
             return tempList;
         }
 
         public void AddTempScheduleToDB(TempSchedule tSchedule)
         {
-            using (DbConnectionADO dBCon = new DbConnectionADO())
+            using (SqlConnection dBCon = new SqlConnection(dbConADO.KrakaConnectionString()))
             {
-                dBCon.OpenConnection();
-                SqlCommand insertTempSchedule = new SqlCommand("INSERT INTO TempSchedule (noOfWeeks, name)  VALUES(@param1,@param2)");
+                dBCon.Open();
+                SqlCommand insertTempSchedule = new SqlCommand("INSERT INTO TempSchedule (noOfWeeks, name)  VALUES(@param1,@param2)", dBCon);
                 
                 insertTempSchedule.Parameters.AddWithValue("@param1", tSchedule.NoOfWeeks);
                 insertTempSchedule.Parameters.AddWithValue("@param2", tSchedule.Name);
                 insertTempSchedule.ExecuteNonQuery();
 
-                dBCon.CloseConnection();
+                dBCon.Close();
             }
         }
 
         public TempSchedule FindTempScheduleByName(string scheduleName)
         {
             TempSchedule tSchedule = new TempSchedule();
-            using (DbConnectionADO dBCon = new DbConnectionADO())
+            using (SqlConnection dBCon = new SqlConnection(dbConADO.KrakaConnectionString()))
             {
-                dBCon.OpenConnection();
-                //TODO: Uncomment
-                //SqlCommand command = new SqlCommand("SELECT TempSchedule FROM TempSchedule WHERE Name = @param1", dBCon);
-                //command.Parameters.AddWithValue("@param1", scheduleName);
+                dBCon.Open();
+                SqlCommand command = new SqlCommand("SELECT TempSchedule FROM TempSchedule WHERE Name = @param1", dBCon);
+                command.Parameters.AddWithValue("@param1", scheduleName);
 
-                //using (DbDataReader reader = command.ExecuteReader())
-                //{
-                //    while (reader.Read())
-                //    {
-                //        tSchedule = new TempSchedule(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2));
-                //    }
-                //}
-                dBCon.CloseConnection();
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        tSchedule = new TempSchedule(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2));
+                    }
+                }
+                dBCon.Close();
             }
             return tSchedule;
         }
--- ServiceHosting/Program.cs
+++ ServiceHosting/Program.cs
@@ -18,11 +18,29 @@ namespace ServiceHosting
             {
                 //open the host and start listening for incoming messages                 
                 serviceHost.Open();
+                DisplayHostInfo(serviceHost);
                 //keep the service running until the Enter key is pressed                 
                 Console.WriteLine("The service is ready.");
                 Console.WriteLine("Press the Enter key to terminate service.");
+                Console.WriteLine(serviceHost.BaseAddresses.ToString());
                 Console.ReadLine();
             }
+
+        }
+        static void DisplayHostInfo(ServiceHost host)
+        {
+            Console.WriteLine();
+            Console.WriteLine("****** Host Info *******");
+            foreach (System.ServiceModel.Description.ServiceEndpoint se in host.Description.Endpoints)
+            {
+                Console.WriteLine("Address: {0}", se.Address);
+                Console.WriteLine("Binding: {0}", se.Binding.Name);
+                Console.WriteLine("Contract: {0}", se.Contract.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("************************");
+
         }
     }
+
 }
