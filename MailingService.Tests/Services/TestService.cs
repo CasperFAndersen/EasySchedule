1fe5c1b14c8446d98cad7160bb6cf299f6af7ec3
T. Andersen <Tzarreth@users.noreply.github.com>
2017-11-14 10:52:47 +0100

    Test
    EmployeeProxy Test

---
+++ MailingService.Tests/Services/TestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MailingService.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailingService.Tests.Services
+{
+    [TestClass]
+    public class TestService
+    {
+        [TestMethod]
+        public void EmployeeServiceTest()
+        {
+            EmployeeProxy empProxy = new EmployeeProxy();
+
+            Assert.AreEqual("Tobias", empProxy.GetEmployeeByUsername("TobMaster"));
+        }
+    }
+}
