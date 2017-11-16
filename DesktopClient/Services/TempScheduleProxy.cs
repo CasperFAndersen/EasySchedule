0bbbc8c31fc18e03d78e6e9d82b4b05ba4ea0c13
Casper Froberg Andersen <casperfroberg@gmail.com>
2017-11-16 08:57:51 +0100

    Service + proxy OB

---
+++ DesktopClient/Services/TempScheduleProxy.cs
@@ -0,0 +1,53 @@
+﻿using DesktopClient.TempScheduleService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClient
+{
+    public class TempScheduleProxy : ITempScheduleService
+    {
+        TempScheduleServiceClient proxy = new TempScheduleServiceClient();
+        public void AddTempScheduleToDB(TempSchedule tSchedule)
+        {
+            proxy.AddTempScheduleToDB(tSchedule);
+        }
+
+        public Task AddTempScheduleToDBAsync(TempSchedule tSchedule)
+        {
+           return proxy.AddTempScheduleToDBAsync(tSchedule);
+        }
+
+        public void AddTempShift(TempShift tShift)
+        {
+            proxy.AddTempShift(tShift);
+        }
+
+        public Task AddTempShiftAsync(TempShift tShift)
+        {
+            return proxy.AddTempShiftAsync(tShift);
+        }
+
+        public TempSchedule FindTempScheduleByName(string name)
+        {
+            return proxy.FindTempScheduleByName(name);
+        }
+
+        public Task<TempSchedule> FindTempScheduleByNameAsync(string name)
+        {
+            return proxy.FindTempScheduleByNameAsync(name);
+        }
+
+        public TempSchedule[] GetAllTempSchedules()
+        {
+            return proxy.GetAllTempSchedules();
+        }
+
+        public Task<TempSchedule[]> GetAllTempSchedulesAsync()
+        {
+            return proxy.GetAllTempSchedulesAsync();
+        }
+    }
+}
---
+++ DesktopClient/Services/TempShiftService.cs
@@ -0,0 +1,33 @@
+﻿using DesktopClient.TempShiftService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopClient.Services
+{
+    public class TempShiftService : ITempShiftService
+    {
+        TempShiftServiceClient proxy = new TempShiftServiceClient();
+        public TempShift CreateTempShift(DateTime weekNumber, int hours, DateTime startTime, Employee shiftEmployee)
+        {
+            return proxy.CreateTempShift(weekNumber, hours, startTime, shiftEmployee);
+        }
+
+        public Task<TempShift> CreateTempShiftAsync(DateTime weekNumber, int hours, DateTime startTime, Employee shiftEmployee)
+        {
+            return proxy.CreateTempShiftAsync(weekNumber, hours, startTime, shiftEmployee);
+        }
+
+        public TempShift FindTempShiftByID(TempSchedule tSchedule, int tempShiftID)
+        {
+            return proxy.FindTempShiftByID(tSchedule, tempShiftID);
+        }
+
+        public Task<TempShift> FindTempShiftByIDAsync(TempSchedule tSchedule, int tempShiftID)
+        {
+            return proxy.FindTempShiftByIDAsync(tSchedule, tempShiftID);
+        }
+    }
+}
