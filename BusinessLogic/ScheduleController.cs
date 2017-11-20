58de14865efeafa3187c396b1688c66f09a97c4b
Stefan Krabbe <stefankrabbe54@gmail.com>
2017-11-20 10:15:34 +0100

    Fixed schedule controller

--- BusinessLogic/ScheduleController.cs
+++ BusinessLogic/ScheduleController.cs
@@ -21,16 +21,16 @@ namespace BusinessLogic
         {
             int day = (int)DayOfWeek.Monday;
             DateTime date = new DateTime(currentDate.Year, currentDate.Month, day);
-            
+
             return schRep.GetScheduleByCurrentDate(date);
         }
 
         public Schedule GetCurrentScheduleByDepartmentId(int id)
         {
-            
+
             DateTime currentDate = DateTime.Now;
             //int day = currentDate.Day - ((int)currentDate.DayOfWeek -1);
-            int day = (currentDate.DayOfWeek == DayOfWeek.Sunday) ? (currentDate.Day - 6) : ((int)currentDate.DayOfWeek - 1);
+            int day = (currentDate.DayOfWeek == DayOfWeek.Sunday) ? (currentDate.Day - 6) : (currentDate.Day - ((int)currentDate.DayOfWeek - 1));
             DateTime date = new DateTime(currentDate.Year, currentDate.Month, day);
 
             return schRep.GetCurrentScheduleByDepartmentId(date, id);
