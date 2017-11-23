800a8e596ebe22266d8c62eea3c32f77a6fd7f93
mikkellpaulsen <mikkellpaulsen@gmail.com>
2017-11-23 18:50:57 +0100

    db connection status method

---
+++ BusinessLogic/DBConnectionControllercs.cs
@@ -0,0 +1,39 @@
+﻿using DatabaseAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class DBConnectionControllercs
+    {
+        public bool DBConnectionStatus()
+        {
+            DbConnectionADO dbConADO = new DbConnectionADO();
+            bool status = false;
+            var thread = new Thread(() =>
+            {
+                using (SqlConnection dBCon = new SqlConnection(dbConADO.KrakaConnectionString()))
+                {
+                   try
+                   {
+                        dBCon.Open();
+                        status = true;
+                   }
+                   catch (SqlException)
+                   {
+                     status = false;
+                   } 
+                }
+            });
+            thread.Start();
+            Thread.Sleep(5000);
+            return status;
+        }
+    }
+}
