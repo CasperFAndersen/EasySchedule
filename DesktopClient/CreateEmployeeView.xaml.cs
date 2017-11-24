ee04cd430fb41817e208c32287d26bcf9d95cb53
Casper Froberg Andersen <casperfroberg@gmail.com>
2017-11-24 10:34:31 +0100

    CreateEmployeeView

--- DesktopClient/CreateEmployeeView.xaml.cs
+++ DesktopClient/CreateEmployeeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,40 @@ namespace DesktopClient
     /// </summary>
     public partial class CreateEmployeeView : UserControl
     {
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private string Phone { get; set; }
+        private int NoOfHOurs { get; set; }
+        private bool IsAdmin { get; set; }
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private List<Department> ListOfDepartments { get; set; }
+
+
         public CreateEmployeeView()
         {
             InitializeComponent();
+            LoadDeparmentList();
+        }
+
+        private void LoadDeparmentList()
+        {
+            List<Department> listOfDepartments = new DepartmentEvents()
+        }
+
+        private void btnSaveEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.Name = txtName.Text;
+            emp.Mail = txtEmail.Text;
+            emp.Phone = txtPhone.Text;
+            emp.NumbOfHours = Convert.ToInt32(txtNoOfHours.Text);
+            emp.IsAdmin = Convert.ToBoolean(chkIsAdmin.IsChecked);
+            emp.Username = txtUsername.Text;
+            emp.Password = txtPassword.Text;
+            Department selectedDepartment = (Department)cBoxDepartment.SelectedItem;
+            emp.DepartmentId = selectedDepartment.Id;
+            //TODO Make the client call the service.
         }
     }
 }
