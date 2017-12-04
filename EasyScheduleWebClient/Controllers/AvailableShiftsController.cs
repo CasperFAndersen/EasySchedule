59d02c87c876f5fe6012ccd18ea40647b1ddb5d2
T. Andersen <Tzarreth@users.noreply.github.com>
2017-12-04 12:36:26 +0100

    Gui til TV

---
+++ EasyScheduleWebClient/Controllers/AvailableShiftsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace EasyScheduleWebClient.Controllers
+{
+    public class AvailableShiftsController : Controller
+    {
+        public List<int> test { get; set; }
+        // GET: AvailableShifts
+        public ActionResult Index()
+        {
+            ServiceLibrary.Schedules.ScheduleService scheduleService = new ServiceLibrary.Schedules.ScheduleService();
+            
+            
+            return View();
+        }
+
+        
+
+
+    }
+}
---
+++ EasyScheduleWebClient/Models/ShiftModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Core;
+
+namespace EasyScheduleWebClient.Models
+{
+    public class ShiftModel
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Hours { get; set; }
+        public Employee Employee { get; set; }
+        public bool IsForSale { get; set; }
+    }
+}
