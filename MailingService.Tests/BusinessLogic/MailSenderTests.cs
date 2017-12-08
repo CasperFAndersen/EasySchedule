d78f2690545650edcd3c3d8c2cf2403ca2015f25
Stefan Krabbe <stefankrabbe54@gmail.com>
2017-12-08 16:59:58 +0100

    Added test for mailsender

---
+++ MailingService.Tests/BusinessLogic/MailSenderTests.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.BusinessLogic
+{
+    [TestClass()]
+    public class MailSenderTests
+    {
+        [TestMethod()]
+        public void SendMailToEmployeesInDepartmentByDepartmentIdTest()
+        {
+            string subject = "Test";
+            string text = "Test";
+            MailSender mailSender = new MailSender();
+            mailSender.SendMailToEmployeesInDepartmentByDepartmentId(subject, text, 1);
+            mailSender.SendMailToEmployeesInDepartmentByDepartmentId(subject, text, 2);
+        }
+    }
+}
